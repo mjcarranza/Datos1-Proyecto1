b9d1e5baea061799f9e4a7c1732b4cbf8b6f062f
Mario <48070830+mjcarranza@users.noreply.github.com>
2019-09-23 22:15:01 -0600

    Update ListaObjetos.java

--- CIrcuit_Designer/src/circuit_designer/ListaObjetos.java
+++ CIrcuit_Designer/src/circuit_designer/ListaObjetos.java
@@ -1,24 +1,53 @@
+/**
+ * Paquete.
+ */
 package circuit_designer;
-
+/**
+ * Se define la clase ListaObjetos para guardar los datos de las compuertas.
+ * @author Mario
+ *
+ */
 public class ListaObjetos {
+	/*
+	 * Definición de variables.
+	 */
     private Node head;
     private int size;
+    /**
+     * Constructor de la clase.
+     */
     public ListaObjetos() {
         this.head = null;
         this.size = 0;
     }
+    /**
+     * Método para saber si la lista está vacía.
+     * @return  devuelve si la lista está vacía.
+     */
     public boolean isEmpty() {
         return this.head == null;
     }
-    public int size() {//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /**
+     * Método que devuelve la longitud de la lista.
+     * @return
+     */
+    public int size() {
         return this.size;
     }
-    public void insertFirst(Object data) {
+    /**
+     * Método para insertar un nuevo valor a la lista (crea un nuevo nodo).
+     * @param data
+     */
+    public void insertFirst(String data) {
         Node newBNode = new Node(data);
         newBNode.next = this.head;
         this.head = newBNode;
         this.size++;
     }
+    /**
+     * Método que se encarga de eliminar el primer nodo de la lista.
+     * @return
+     */
     public Node deleteFirst() {
         if (this.head != null) {
             Node temp = this.head;
@@ -29,17 +58,38 @@ public class ListaObjetos {
             return null;
         }
     }
+    /**
+     * Método para modificar los nodos.
+     * @param dato Recibe el dato a modificar como parámetro.
+     */
+    public void modify(Object dato) {
+    	Node current = this.head;
+    	while(current != null) {
+    		if (current.getData() == dato) {
+    			current = (Node) dato;
+    		}
+    	}
+    }
+    /**
+     * Método para imprimir la lista completa.
+     */
     public void displayList() {
         Node current = this.head;
+        System.out.println("1: " + current.getData());
         while (current != null) {
             System.out.println(current.getData());
             current = current.getNext();
         }
     }
-    public Node find(String searchValue) {
+    /**
+     * Método que busca un nodo en específico dentro de la lista.
+     * @param data
+     * @return
+     */
+    public Node find(String data) {
         Node current = this.head;
         while (current != null) {
-            if (current.getData().equals(searchValue)) {
+            if (current.getData().equals(data)) {
                 return current;
             } else {
                 current = current.getNext();
@@ -47,6 +97,11 @@ public class ListaObjetos {
         }
         return null;
     }
+    /**
+     * Método para eliminar un nodo en específico de la lista.
+     * @param searchValue
+     * @return
+     */
     public Node delete(String searchValue) {
         Node current = this.head;
         Node previous = this.head;
@@ -67,21 +122,4 @@ public class ListaObjetos {
         return null;
     }
 
-    public static void main(String[] args){
-        ListaObjetos lList = new ListaObjetos();
-        lList.insertFirst("Dato1");
-        lList.insertFirst("Dato2");
-        lList.insertFirst("Dato3");
-        lList.displayList();
-        lList.deleteFirst();
-        System.out.println();
-        lList.displayList();
-        lList.insertFirst("Dato4");
-        lList.insertFirst("Dato5");
-        lList.insertFirst("Dato6");
-        System.out.println("Dato encontrado: " + lList.find("Dato5").getData());
-
-        System.out.println("Dato borrado: " + lList.delete("Dato5").getData());
-        lList.displayList();
-    }
 }
